868c198d904f7ec067f46643a37b1644837f5c43
Wiibleyde <nathan@bonnell.fr>
2024-04-24 17:10:55 +0200

    We can start games ! (but not play in front for now)
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def getCurrentGame() -> str:
         if game and player:
             hiddenGame = hideSensitiveDatas(game)
             return flask.jsonify(hiddenGame.__dict__())
-    return "No game found"
+    return flask.jsonify({"error": "Player or game not found"})
 
 @app.route("/joinGame", methods=["POST"])
 def joinGame():
@@ -71,20 +71,20 @@ def joinGame():
     if playerUuid:
         player = getPlayer(playerUuid)
         if player:
-            return "Player already in a game"
+            return flask.jsonify({"error": "Player already in a game"})
     player = playerObj.Player(uuid=str(uuid.uuid4()), pseudo=pseudo)
     game = getGame(gameId)
     if game:
         errMessage = game.addPlayer(player)
         if errMessage:
-            return errMessage.message
+            return flask.jsonify({"error": errMessage.message})
         if len(game.players) == 1:
             game.host = player
-        resp = flask.make_response("Player joined")
+        resp = flask.make_response(flask.jsonify(game.__dict__()))
         resp.set_cookie("playerUWUID", player.uuid, expires=datetime.datetime.now() + datetime.timedelta(hours=1))
         resp.set_cookie("gameUWUID", game.uuid, expires=datetime.datetime.now() + datetime.timedelta(hours=1))
         return resp
-    return "Game not found"
+    return flask.jsonify({"error": "Game not found"})
 
 @app.route("/leaveGame", methods=["POST"])
 def leaveGame():
@@ -97,11 +97,11 @@ def leaveGame():
             errMessage = game.removePlayer(player)
             if errMessage:
                 return errMessage.message
-            resp = flask.make_response("Player left")
+            resp = flask.make_response(flask.jsonify({"status": "Player left"}))
             resp.set_cookie("playerUWUID", "", expires=0)
             return resp
-        return "Player not found"
-    return "Game not found"
+        return flask.jsonify({"error": "Player not found"})
+    return flask.jsonify({"error": "Game not found"})
 
 @app.route("/startGame", methods=["POST"])
 def startGame():
@@ -109,11 +109,12 @@ def startGame():
     gameUuid = flask.request.cookies.get("gameUWUID")
     game = getGame(gameUuid)
     if game:
-        errMessage = game.startGame(playerUuid)
+        player = getPlayerInGame(playerUuid, gameUuid)
+        errMessage = game.startGame(player)
         if errMessage is None:
-            return "Game started"
-        return errMessage.message
-    return "Game not found"
+            return flask.jsonify({"status": "Game started"})
+        return flask.jsonify({"error": errMessage.message})
+    return flask.jsonify({"error": "Game not found"})
 
 @app.route("/playDescTurn", methods=["POST"])
 def playDescTurn():
@@ -126,10 +127,10 @@ def playDescTurn():
             desc = flask.request.args.get("desc")
             errMessage = game.playDesc(player, desc)
             if errMessage is None:
-                return "Description played"
-            return errMessage.message
-        return "Player not found"
-    return "Game not found"
+                return flask.jsonify({"status": "Description played"})
+            return flask.jsonify({"error": errMessage.message})
+        return flask.jsonify({"error": "Player not found"})
+    return flask.jsonify({"error": "Game not found"})
 
 @app.route("/playVoteTurn", methods=["POST"])
 def playVoteTurn():
@@ -142,10 +143,10 @@ def playVoteTurn():
             vote = flask.request.args.get("vote")
             errMessage = game.playVote(player, vote)
             if errMessage is None:
-                return "Vote played"
-            return errMessage.message
-        return "Player not found"
-    return "Game not found"
+                return flask.jsonify({"status": "Vote played"})
+            return flask.jsonify({"error": errMessage.message})
+        return flask.jsonify({"error": "Player not found"})
+    return flask.jsonify({"error": "Game not found"})
 
 @app.route("/playDiscussionTurn", methods=["POST"])
 def playDiscussionTurn():
@@ -157,11 +158,11 @@ def playDiscussionTurn():
         if player:
             errMessage = game.playDiscussion(player)
             if errMessage is None:
-                return "Discussion played"
-            return errMessage.message
-        return "Player not found"
-    return "Game not found"
+                return flask.jsonify({"status": "Discussion played"})
+            return flask.jsonify({"error": errMessage.message})
+        return flask.jsonify({"error": "Player not found"})
+    return flask.jsonify({"error": "Game not found"})
 
 if __name__ == "__main__":
     csvUtil.getWords()
-    app.run()
+    app.run(debug=True)
